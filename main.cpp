4ca63cf25f476772c1527b83b1f6a9b887b1222f
mateonoel2 <39866695+mateonoel2@users.noreply.github.com>
2019-08-22 14:56:18 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,46 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int process(int n){
+    int contador = 1;
+    while (n != 1) {
+        if (n % 2 != 0) {
+            n = 3 * n + 1;
+            contador+=1;
+        }
+        else {
+            n = n / 2;
+            contador+=1;
+        }
+    }
+    return contador;
+}
+
+int extraNumber(int i, int j){
+    int max = 0;
+    if(i<j){
+        for(int n=i; n<=j ;n++){
+            if (max < process(n)){
+              max=process(n);
+            }
+        }
+        return max;
+    }
+    else{
+        for(int n=j; n<=i ;n++){
+            if (max < process(n)){
+                max=process(n);
+            }
+        }
+        return max;
+    }
+}
+
+int main() {
+   int i, j;
+    while(cin>>i>>j){
+        cout<< i<<" "<< j<<" ";
+        cout<<extraNumber(i, j)<<'\n';
+    }
     return 0;
 }
